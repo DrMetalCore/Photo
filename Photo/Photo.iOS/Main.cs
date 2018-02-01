b48f0d57250be7d3b4cfd9cc3325d46eaa030d5a
Luka Moraiz <lmoraiz@iutbayonne.univ-pau.fr>
2018-02-01 16:50:34 +0100

    V1.0

--- Photo/Photo.iOS/Main.cs
+++ Photo/Photo.iOS/Main.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿
+using Plugin.Media;
 using UIKit;
 
 namespace Photo.iOS
@@ -10,11 +7,13 @@ namespace Photo.iOS
     public class Application
     {
         // This is the main entry point of the application.
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task MainAsync(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
+
+            await CrossMedia.Current.Initialize();
         }
     }
 }
--- Photo/Photo/MainPage.xaml.cs
+++ Photo/Photo/MainPage.xaml.cs
@@ -1,17 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using Xamarin.Forms;
 
 namespace Photo
 {
 	public partial class MainPage : ContentPage
 	{
+
 		public MainPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+        public async void PrendrePhoto_Clicked(object sender, EventArgs e)
+        {
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Test",
+                SaveToAlbum = true,
+                DefaultCamera = CameraDevice.Front
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        public async void ChoisirPhoto_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                return;
+            }
+            var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+
+            });
+
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+    }
 }
